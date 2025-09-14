6802ce622a62c96e69a08f2434028053b2959746
SharmaDevanshu089 <sharma.devanshu089@gmail.com>
2025-09-14 13:23:46 +0530

    Added Functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::io;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn get_in() -> String{
+    let mut flag = "ok";
+    let mut string_to_get_in = String::new();
+    let mut string_to_return = String::new();
+    match io::stdin().read_line(&mut string_to_get_in){
+        Ok(_) => string_to_return = string_to_get_in,
+        Err(_) => flag = "er",
     }
+    // TODO : Add a flag system which will alert the system if there is any problem
+    return string_to_return;
 }
